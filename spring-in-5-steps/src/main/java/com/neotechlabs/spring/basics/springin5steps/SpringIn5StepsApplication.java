4def972737d322d3794da6d912f9729648fb6bed
Azad Asanali <azadxperias2012@gmail.com>
2017-12-17 14:19:40 +0530

    bean scope - change default singleton bean scope to prototype

--- spring-in-5-steps/src/main/java/com/neotechlabs/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/neotechlabs/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -7,12 +7,19 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
-	public static void main(String[] args) {		
-		ApplicationContext applicationContext =
-				SpringApplication.run(SpringIn5StepsApplication.class, args);
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
-		int result = binarySearch.binarySearch(new int[] {1,2,3,4}, 3);
+		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+
+		System.out.println(binarySearch);
+		System.out.println(binarySearch1);
+
+		int result = binarySearch.binarySearch(new int[] { 1, 2, 3, 4 }, 3);
+		int result1 = binarySearch.binarySearch(new int[] { 1, 2, 3, 4 }, 3);
 		System.out.println(result);
+		System.out.println(result1);
 	}
 
 }
