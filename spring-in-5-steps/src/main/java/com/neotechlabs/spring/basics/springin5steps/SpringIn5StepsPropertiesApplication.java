9e38cf95b2c43f4871504c967534552ef9054f2d
Azad Asanali <azadxperias2012@gmail.com>
2018-03-09 09:52:10 +0530

    Step 26 - Read values from external properties

---
+++ spring-in-5-steps/src/main/java/com/neotechlabs/spring/basics/springin5steps/SpringIn5StepsPropertiesApplication.java
@@ -0,0 +1,26 @@
+package com.neotechlabs.spring.basics.springin5steps;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.neotechlabs.spring.basics.springin5steps.basic.BinarySearchImpl;
+import com.neotechlabs.spring.basics.springin5steps.properties.SomeExternalService;
+
+@Configuration
+@ComponentScan
+@PropertySource("classpath:app.properties")
+public class SpringIn5StepsPropertiesApplication {
+
+	public static void main(String[] args) {
+		try (AnnotationConfigApplicationContext applicationContext =
+				new AnnotationConfigApplicationContext(
+				SpringIn5StepsPropertiesApplication.class)) {
+
+			SomeExternalService someExternalService = applicationContext.getBean(SomeExternalService.class);
+			System.out.println(someExternalService);
+			System.out.println(someExternalService.returnServiceUrl());
+		}
+	}
+}
