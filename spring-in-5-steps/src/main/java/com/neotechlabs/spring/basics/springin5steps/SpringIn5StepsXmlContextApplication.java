9953a24353967d2c5cafb4121f32e45865414541
Azad Asanali <azadxperias2012@gmail.com>
2018-03-09 09:20:14 +0530

    Step 23 - Mixing XML Context with Component Scan for Beans defined with Annotation

--- spring-in-5-steps/src/main/java/com/neotechlabs/spring/basics/springin5steps/SpringIn5StepsXmlContextApplication.java
+++ spring-in-5-steps/src/main/java/com/neotechlabs/spring/basics/springin5steps/SpringIn5StepsXmlContextApplication.java
@@ -2,16 +2,10 @@ package com.neotechlabs.spring.basics.springin5steps;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.neotechlabs.spring.basics.springin5steps.basic.BinarySearchImpl;
 import com.neotechlabs.spring.basics.springin5steps.xml.XmlPersonDAO;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXmlContextApplication {
 	
 	static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsXmlContextApplication.class);
@@ -21,9 +15,11 @@ public class SpringIn5StepsXmlContextApplication {
 		try (ClassPathXmlApplicationContext applicationContext =
 				new ClassPathXmlApplicationContext("applicationContext.xml")) {
 			
-			XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);
-			LOGGER.info("{}", personDao);
-			LOGGER.info("{}", personDao.getXmlJdbcConnection());
+			LOGGER.info("Beans Loaded -> {}",
+					(Object) applicationContext.getBeanDefinitionNames());
+			
+			XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);			
+			LOGGER.info("{} {}", personDao, personDao.getXmlJdbcConnection());
 		}
 			
 	}
