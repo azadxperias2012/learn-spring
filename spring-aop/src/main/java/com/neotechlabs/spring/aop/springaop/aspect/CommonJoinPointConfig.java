f8e2542fa88816ed5be09a2eb02c095a2cce503f
Azad Asanali <azadxperias2012@gmail.com>
2018-03-18 07:49:07 +0530

    Step 08 - Quick summary of other Pointcuts

--- spring-aop/src/main/java/com/neotechlabs/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ spring-aop/src/main/java/com/neotechlabs/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -9,5 +9,14 @@ public class CommonJoinPointConfig {
 	
 	@Pointcut("execution(* com.neotechlabs.spring.aop.springaop.business.*.*(..))")
 	public void businessLayerExecution() {}
+	
+	@Pointcut("com.neotechlabs.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.neotechlabs.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+	public void allLayerExecution() {}
+	
+	@Pointcut("bean(*dao*)")
+	public void beanContainingDao() {}
+	
+	@Pointcut("within(* com.neotechlabs.spring.aop.springaop.data..*)")
+	public void dataLayerExecutionWithWithin() {}
 
 }
