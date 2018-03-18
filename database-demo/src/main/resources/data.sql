7f782e2024ea5d8b8733a00ed4ee30fb64500bae
Azad Asanali <azadxperias2012@gmail.com>
2018-03-18 12:12:04 +0530

    Spring JDBC - Step 06 - Execute the findAll method using CommandLineRunner

---
+++ database-demo/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+create table person
+(
+	id integer not null,
+	name varchar(255) not null,
+	location varchar(255),
+	birth_date timestamp,
+	primary key(id)
+);
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) 
+VALUES(10001, 'Azad', 'Trivandrum', sysdate());
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) 
+VALUES(10002, 'Abi', 'Chennai', sysdate());
+
+INSERT INTO PERSON (ID, NAME, LOCATION, BIRTH_DATE) 
+VALUES(10003, 'Ruhan', 'Sydney', sysdate());
