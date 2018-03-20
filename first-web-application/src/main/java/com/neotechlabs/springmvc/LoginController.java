17a68142a5b286e92ab147bf9acde07a3e0f2454
Azad Asanali <azadxperias2012@gmail.com>
2018-03-20 22:17:22 +0530

    Step 12 : Your first Spring MVC Controller

---
+++ first-web-application/src/main/java/com/neotechlabs/springmvc/LoginController.java
@@ -0,0 +1,16 @@
+package com.neotechlabs.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value="/login")
+	@ResponseBody
+	public String sayHello() {
+		return "Hello World from LoginController1";
+	}
+
+}
