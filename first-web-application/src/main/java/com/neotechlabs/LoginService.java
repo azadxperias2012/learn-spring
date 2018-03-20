2cadfad0b25d3609797b877335dab6dc148c4004
Azad Asanali <azadxperias2012@gmail.com>
2018-03-20 21:36:01 +0530

    Step 10 : Setting up maven, tomcat and simple jee application

--- first-web-application/src/main/java/com/neotechlabs/LoginService.java
+++ first-web-application/src/main/java/com/neotechlabs/LoginService.java
@@ -3,11 +3,8 @@ package com.neotechlabs;
 public class LoginService {
 	
 	public boolean isUserValid(String user, String password) {
-		if (user.equals("neotechlabs") &&
-				password.equals("Welcome1")) {
-			return true;
-		}
-		return false;
+		return (user.equals("neotechlabs") &&
+				password.equals("Welcome1"));
 	}
 
 }
