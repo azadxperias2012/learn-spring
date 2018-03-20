87dccc4b679027b5338b9e27c7c9bac42f005842
Azad Asanali <azadxperias2012@gmail.com>
2018-03-20 21:33:00 +0530

    Modified file name to LoginService

--- first-web-application/src/main/java/com/neotechlabs/LoginService.java
+++ first-web-application/src/main/java/com/neotechlabs/LoginService.java
@@ -1,6 +1,6 @@
 package com.neotechlabs;
 
-public class UserValidationService {
+public class LoginService {
 	
 	public boolean isUserValid(String user, String password) {
 		if (user.equals("neotechlabs") &&
